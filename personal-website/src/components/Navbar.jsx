9f2e8ee5c999e617995b76e6c25034dc92428d5b
Kartikay Sharma <kartikaysharma6840@gmail.com>
2024-02-16 01:42:50 +0530

    Update Navbar.jsx

--- personal-website/src/components/Navbar.jsx
+++ personal-website/src/components/Navbar.jsx
@@ -16,24 +16,31 @@ const Navbar = () => {
   }
 
   window.addEventListener('scroll', scrolling)
+
+  const linkStyle = {
+    textDecoration: 'none',
+    fontSize: '1.5rem',
+    color: '#fff',
+  };
+
   return (
     <>
         <div className={color ? "nav nav-bg" : "nav"}>
           <ul className='nav-list'>
               <li className='font3'>
-                <Link to='about' smooth={true} duration={10}>
+                <Link to='about' smooth={true} duration={100} style={linkStyle}>
                   About
                 </Link></li>
               <li className='font3'>
-                <Link to='projects' smooth={true} duration={10}>
+                <Link to='projects' smooth={true} duration={100} style={linkStyle}>
                   Projects
                 </Link></li>
               <li className='font3'>
-                <Link to='skills' smooth={true} duration={10}>
+                <Link to='skills' smooth={true} duration={100} style={linkStyle}>
                   Skills
                 </Link></li>
               <li className='font3'>
-                <Link to='contact'smooth={true} duration={10} >
+                <Link to='contact'smooth={true} duration={100} style={linkStyle} >
                   Contact
                 </Link></li>
           </ul>
